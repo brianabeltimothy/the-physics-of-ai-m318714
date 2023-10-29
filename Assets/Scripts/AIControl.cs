60c4539ab77fb976fb3fe8df21c4aa8eea9aa8bd
brianabeltimothy <brianabeltimothyd@gmail.com>
2023-10-29 16:12:16 +0000

    crowd simulation - creating a city crowd

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -5,16 +5,30 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
+
 
     void Start() {
 
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        float sm = Random.Range(0.5f, 2);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
     }
 
     void Update() {
-
+        if(agent.remainingDistance < 1)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
