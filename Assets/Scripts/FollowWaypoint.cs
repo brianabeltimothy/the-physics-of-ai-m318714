fbdbb6c597b72fca6a44af43110dba9c24705d0c
brianabeltimothy <brianabeltimothyd@gmail.com>
2023-11-08 12:56:45 +0000

    waypoints & graphs - following a tracker and circuit

--- Assets/Scripts/FollowWaypoint.cs
+++ Assets/Scripts/FollowWaypoint.cs
@@ -4,19 +4,26 @@ public class FollowWaypoint : MonoBehaviour
 {
     public GameObject[] waypoints;
     private int currentWaypoint = 0;
-    private float tankSpeed = 10.0f;
+    public float tankSpeed = 10.0f;
     public float rotationSpeed = 3.0f;
+    GameObject tracker; 
+    private float lookAhead = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if(Vector3.Distance(this.transform.position, waypoints[currentWaypoint].transform.position) < 3)
+        if(Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if(Vector3.Distance(tracker.transform.position, waypoints[currentWaypoint].transform.position) < 3)
         {
             currentWaypoint++;
         }
@@ -26,9 +33,18 @@ public class FollowWaypoint : MonoBehaviour
             currentWaypoint = 0;
         }
 
-        Quaternion lookAtWaypoint = Quaternion.LookRotation(waypoints[currentWaypoint].transform.position - transform.position);
+        tracker.transform.LookAt(waypoints[currentWaypoint].transform);
+        tracker.transform.Translate(0, 0, (tankSpeed + 2) * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
+
+        Quaternion lookAtWaypoint = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
+
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtWaypoint, rotationSpeed * Time.deltaTime);
-        
-        transform.Translate(0, 0, tankSpeed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, tankSpeed * Time.deltaTime);
     }
 }
