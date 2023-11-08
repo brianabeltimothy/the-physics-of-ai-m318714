647d622eff22dca8f178523413196ea89ef86c78
brianabeltimothy <brianabeltimothyd@gmail.com>
2023-11-08 00:27:18 +0000

    waypoints & graphs - waypoints

--- Assets/Scripts/FollowWaypoint.cs
+++ Assets/Scripts/FollowWaypoint.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowWaypoint : MonoBehaviour
 {
     public GameObject[] waypoints;
     private int currentWaypoint = 0;
-
     private float tankSpeed = 10.0f;
+    public float rotationSpeed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +21,14 @@ public class FollowWaypoint : MonoBehaviour
             currentWaypoint++;
         }
 
-        // if(currentWaypoint >= waypoints.Length())
-        // {
-        //     currentWaypoint = 0;
-        // }
+        if(currentWaypoint >= waypoints.Length)
+        {
+            currentWaypoint = 0;
+        }
 
-        // transform.position 
+        Quaternion lookAtWaypoint = Quaternion.LookRotation(waypoints[currentWaypoint].transform.position - transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtWaypoint, rotationSpeed * Time.deltaTime);
+        
+        transform.Translate(0, 0, tankSpeed * Time.deltaTime);
     }
 }
